826d7d1248b1c2dd1ff4c9228fa5ec444324b0ec
BuiHoanDuy <hoanduy1802@gmail.com>
2024-06-01 13:21:32 +0700

    Sua phan doc ghi file, them Game da dong goi

--- src/gui/HighScore.java
+++ src/gui/HighScore.java
@@ -4,7 +4,10 @@ import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class HighScore {
@@ -20,10 +23,29 @@ public class HighScore {
 
         label = new Btn("HIGH SCORE", 25, 420, 380);
 
-        try (BufferedReader br = new BufferedReader(new FileReader("save/score.txt"))) {
+        // Kiểm tra và tạo thư mục 'save' nếu chưa tồn tại
+        File saveDir = new File("save");
+        if (!saveDir.exists()) {
+            saveDir.mkdirs();
+        }
+
+        // Kiểm tra và tạo tệp 'score.txt' nếu chưa tồn tại
+        File scoreFile = new File(saveDir, "score.txt");
+        if (!scoreFile.exists()) {
+            try {
+                scoreFile.createNewFile();
+                try (BufferedWriter writer = new BufferedWriter(new FileWriter(scoreFile))) {
+                    writer.write("0"); // Ghi điểm ban đầu là 0
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        try (BufferedReader br = new BufferedReader(new FileReader(scoreFile))) {
             String scoreText;
             while ((scoreText = br.readLine()) != null) {
-                // 36 = 26 + 10 với 26 là chiều rộng một chữ cái size 60, 10 mà khoảng cách giữa 2 chữ
+                // 36 = 26 + 10 với 26 là chiều rộng một chữ cái size 60, 10 là khoảng cách giữa 2 chữ
                 // tính tọa độ x để căn giữa score text
                 int x = (1008 - (36 * scoreText.length())) / 2;
                 score = new Btn(scoreText, 60, x, 450);
